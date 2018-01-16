9dcf5dd8502375e041d2c7fc11aa2df67b42d3f3
Tibor Simko <tibor.simko@cern.ch>
2018-01-16 17:46:09 +0100

    installation: Python-3.6
    * Upgrades Python version to 3.6 for Travis CI.
    
    * Does not declare any Python programming language in `setup.py` Trove
      classifiers though, since the component only runs RabbitMQ.
    
    * Amends wrong copyright statements (Invenio -> REANA).
    
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>

--- docs/conf.py
+++ docs/conf.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# This file is part of Invenio.
-# Copyright (C) 2017 CERN.
+# This file is part of REANA.
+# Copyright (C) 2017, 2018 CERN.
 #
-# Invenio is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
+# REANA is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# Invenio is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# REANA is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with REANA; if not, see <http://www.gnu.org/licenses>.
 #
 # In applying this license, CERN does not waive the privileges and immunities
-# granted to it by virtue of its status as an Intergovernmental Organization
-# or submit itself to any jurisdiction.
+# granted to it by virtue of its status as an Intergovernmental Organization or
+# submit itself to any jurisdiction.
 
 from __future__ import print_function
 
 import os
+
 import sphinx.environment
 
 # If extensions (or modules to document with autodoc) are in another directory,
