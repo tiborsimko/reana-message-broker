6d135dbbad2fd712a1a0005d53929fd4dcf25a15
Tibor Šimko <tibor.simko@cern.ch>
2026-03-12 11:39:15 +0100

    ci(yamllint): add YAML linting checks (#82)

--- run-tests.sh
+++ run-tests.sh
@@ -83,6 +83,10 @@ lint_shellcheck() {
     find . -name "*.sh" -exec shellcheck {} \+
 }
 
+lint_yamllint() {
+    yamllint .
+}
+
 python_tests() {
     pytest
 }
@@ -98,6 +102,7 @@ all() {
     lint_manifest
     lint_pydocstyle
     lint_shellcheck
+    lint_yamllint
     python_tests
 }
 
@@ -116,6 +121,7 @@ help() {
     echo "  --lint-manifest      Check linting of Python manifest"
     echo "  --lint-pydocstyle    Check linting of Python docstrings"
     echo "  --lint-shellcheck    Check linting of shell scripts"
+    echo "  --lint-yamllint      Check linting of YAML files"
     echo "  --python-tests       Check Python test suite"
 }
 
@@ -138,6 +144,7 @@ case $arg in
 --lint-manifest) lint_manifest ;;
 --lint-pydocstyle) lint_pydocstyle ;;
 --lint-shellcheck) lint_shellcheck ;;
+--lint-yamllint) lint_yamllint ;;
 --python-tests) python_tests ;;
 *) echo "[ERROR] Invalid argument '$arg'. Exiting." && help && exit 1 ;;
 esac
